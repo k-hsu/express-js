e3d9b9277526ddcb333e8c2210fa2fd5724d11f3
k-hsu <khsuzmail@gmail.com>
2023-03-21 11:36:12 -0500

    created hello world web server

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const livereload = require("livereload");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+if (app.get("env") === "development") {
+  const liveReloadServer = livereload.createServer();
+  liveReloadServer.watch(path.join(__dirname, "public"));
+
+  app.use(require("connect-livereload")());
+
+  liveReloadServer.server.once("connection", () => {
+    setTimeout(() => {
+      liveReloadServer.refresh("/");
+    }, 100);
+  });
+}
+
+app.use(express.static("public"));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
